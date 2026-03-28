f9a3df7e6a0221f89dd9c8a7bbae67bc7f6d81af
17app001 <17app001@gmail.com>
2026-03-28 14:36:55 +0800

    完成todo代辦事項

--- Chapter4/TodoApp/TodoApp/Program.cs
+++ Chapter4/TodoApp/TodoApp/Program.cs
@@ -6,13 +6,8 @@
         {
             Console.WriteLine("代辦事項APP");
             List<string> todos = new List<string>();
-            //新增
-            todos.Add("去買牛奶");
-            todos.Add("學習C#");
-            todos.Add("去繳電費");
 
             Console.WriteLine($"目前共有{todos.Count}個代辦事項");
-
 
             while (true)
             {
@@ -30,6 +25,10 @@
                 try
                 {
                     choice = int.Parse(Console.ReadLine()!);
+                    if (choice < 1 || choice > 5)
+                    {
+                        Console.WriteLine("輸入選項錯誤，請重新輸入!");
+                    }
                 }
                 catch
                 {
@@ -49,13 +48,86 @@
                     Console.WriteLine($"新增事項成功!目前共有{todos.Count}個代辦事項");
                 }
 
+                //檢視功能
                 if (choice == 2)
                 {
+                    if (todos.Count == 0)
+                    {
+                        Console.WriteLine("目前無代辦事項，請先新增。");
+                        continue;
+                    }
+
                     for (int i = 0; i < todos.Count; i++)
                     {
                         Console.WriteLine($"事項{i + 1}:{todos[i]}");
                     }
                 }
+
+                //更新功能
+                if (choice == 3)
+                {
+                    if (todos.Count == 0)
+                    {
+                        Console.WriteLine("目前無代辦事項，請先新增。");
+                        continue;
+                    }
+
+                    Console.Write("請輸入修改編號:");
+                    if (int.TryParse(Console.ReadLine()!, out int index))
+                    {
+                        //轉換成實際索引值
+                        index--;
+                        //確認索引在範圍之內
+                        if (index < 0 || index >= todos.Count)
+                        {
+                            Console.WriteLine("無此代辦事項編號");
+                        }
+                        else
+                        {
+                            Console.WriteLine("請輸入更新事項:");
+                            string todo = Console.ReadLine()!;
+                            todos[index] = todo;
+                            Console.WriteLine($"更新事項{index + 1}成功!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("輸入編號不正確");
+                    }
+                }
+
+                //刪除功能
+                if (choice == 4)
+                {
+                    if (todos.Count == 0)
+                    {
+                        Console.WriteLine("目前無代辦事項，請先新增。");
+                        continue;
+                    }
+
+                    Console.Write("請輸入刪除編號:");
+                    if (int.TryParse(Console.ReadLine()!, out int index))
+                    {
+                        //轉換成實際索引值
+                        index--;
+                        //確認索引在範圍之內
+                        if (index < 0 || index >= todos.Count)
+                        {
+                            Console.WriteLine("無此代辦事項編號");
+                        }
+                        else
+                        {
+                            todos.RemoveAt(index);
+                            Console.WriteLine($"刪除事項{index + 1}成功!目前剩下{todos.Count}個代辦事項");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("輸入編號不正確");
+                    }
+                }
+
+
 
                 Console.ReadKey();
             }
